51eea54fbe3d664a1e34f79e75647fae6f8ac6d7
naroburnett <68827656+naroburnett@users.noreply.github.com>
2023-02-20 21:28:09 -0700

    information pull
    add a feature to view other users info. I also added a feature that allows the user to edit their account information. Needs checking.

--- app/login/controllers/account.js
+++ app/login/controllers/account.js
@@ -13,3 +13,71 @@ exports.allAccess = (req, res) => {
   exports.moderatorBoard = (req, res) => {
     res.status(200).send("Moderator Content.");
   };
+
+  exports.viewProfile = (req, res) => {
+    const id = req.params.id;
+    Account.findByPk(id)
+      .then(account => {
+        if (!account) {
+          return res.status(404).send({ message: "Account not found" });
+        }
+  
+        // Only return certain fields from the account object
+        const profile = {
+          id: account.id,
+          username: account.username,
+          email: account.email,
+          firstName: account.firstName,
+          lastName: account.lastName,
+          city: account.city,
+          phone: account.phone,
+          gender: account.gender,
+          orientation: account.orientation,
+          dateOfBirth: account.dateOfBirth
+        };
+  
+        res.status(200).send({ profile: profile });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).send({ message: err.message });
+      });
+  };
+
+  exports.getAccount = (req, res) => {
+    Account.findOne({
+      where: { id: req.userId },
+      attributes: { exclude: ["password"] }
+    })
+      .then(account => {
+        if (!account) {
+          return res.status(404).send({ message: "Account not found." });
+        }
+  
+        res.status(200).send(account);
+      })
+      .catch(err => {
+        res.status(500).send({ message: err.message });
+      });
+  };
+  
+  exports.updateAccount = (req, res) => {
+    Account.findOne({ where: { id: req.userId } }).then(account => {
+      if (!account) {
+        return res.status(404).send({ message: "Account not found." });
+      }
+  
+      account.email = req.body.email;
+      account.username = req.body.username;
+  
+      account.save()
+        .then(() => {
+          res.status(200).send({ message: "Account updated successfully!" });
+        })
+        .catch(err => {
+          res.status(500).send({ message: err.message });
+        });
+    });
+  };
+ 
+  
--- app/login/middleware/authJwt.js
+++ app/login/middleware/authJwt.js
@@ -2,6 +2,7 @@ const jwt = require("jsonwebtoken");
 const config = require("../../config/auth");
 const db = require("../models");
 const User = db.user;
+
 verifyToken = (req, res, next) => {
  let token = req.headers["x-access-token"];
  if (!token) {
