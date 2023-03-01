a8c79caec41e047475cf8810c094bcdbacfe1ff8
rgibbons131 <richiegibbons99@gmail.com>
2023-03-01 09:06:14 -0700

    fixed typos

--- app/login/middleware/verifySignUp.js
+++ app/login/middleware/verifySignUp.js
@@ -3,18 +3,6 @@ const account = db.account;
 checkDuplicateUsernameOrEmail = (req, res, next) => {
 
   try {
-   account.findOne({
-      where: {
-         email: req.body.email
-      }
-      }).then(account => {
-      if (account) {
-      res.status(400).send({
-      message: "Failed! Username is already in use!"
-      });
-      return;
-      }});
-      // Email
       account.findOne({
       where: {
       email: req.body.email
