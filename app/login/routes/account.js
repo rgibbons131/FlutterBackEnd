4c4514d4b0872baddd8d2340b62b2f643e71fcf0
naroburnett <68827656+naroburnett@users.noreply.github.com>
2023-03-01 08:04:37 -0700

    Sign out (#6)
    * sign-out
    
    added sign out
    
    * information pull
    
    add a feature to view other users info. I also added a feature that allows the user to edit their account information. Needs checking.
    
    ---------
    
    Signed-off-by: Richard Gibbons <97462610+rgibbons131@users.noreply.github.com>
    Co-authored-by: Richard Gibbons <97462610+rgibbons131@users.noreply.github.com>

--- app/login/routes/account.js
+++ app/login/routes/account.js
@@ -1,5 +1,6 @@
 const { authJwt } = require("../middleware");
 const controller = require("../controllers/account.js");
+
 module.exports = function(app) {
  app.use(function(req, res, next) {
  res.header(
@@ -9,3 +10,16 @@ module.exports = function(app) {
  next();
  });
 };
+
+module.exports = function(app) {
+    app.use(function(req, res, next) {
+      res.header(
+        "Access-Control-Allow-Headers",
+        "x-access-token, Origin, Content-Type, Accept"
+      );
+      next();
+    });
+  
+    app.get("/api/account", [authJwt.verifyToken], controller.getAccount);
+    app.put("/api/account", [authJwt.verifyToken], controller.updateAccount);
+  };
